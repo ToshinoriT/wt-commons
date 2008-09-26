d87c71cb0b472fac7b7694911154746a7d19841f
phil.quitslund <phil.quitslund@a2151643-f252-0410-9a6e-fd29858f4526>
2008-09-26 19:37:47 +0000

    Added Mac OS Preference Page invocation special-handling.

--- trunk/trunk/wt.commons.snippets/src/wt/commons/snippets/eclipse/Snippet003PreferencePageAsserts.java
+++ trunk/trunk/wt.commons.snippets/src/wt/commons/snippets/eclipse/Snippet003PreferencePageAsserts.java
@@ -1,6 +1,7 @@
 package wt.commons.snippets.eclipse;
 
 import com.windowtester.runtime.IUIContext;
+import com.windowtester.runtime.WT;
 import com.windowtester.runtime.WaitTimedOutException;
 import com.windowtester.runtime.WidgetSearchException;
 import com.windowtester.runtime.locator.MenuItemLocator;
@@ -30,6 +31,11 @@ public class Snippet003PreferencePageAsserts extends UITestCaseSWT {
 	private static final boolean AUTO_SAVE_ENABLED = false;
 	private static final boolean AUTO_REFRESH_ENABLED = false;
 
+	
+	//Mac OS requires some special menu handling
+    private static final boolean IS_MAC_OS = System.getProperty("os.name").startsWith("Mac");
+
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -41,11 +47,26 @@ public class Snippet003PreferencePageAsserts extends UITestCaseSWT {
 
 	private void openWorkbenchPreferences(IUIContext ui)
 			throws WidgetSearchException, WaitTimedOutException {
-		ui.click(new MenuItemLocator("Window/Preferences"));
-		ui.wait(new ShellShowingCondition("Preferences"));
-		ui.click(new FilteredTreeItemLocator("General/Workspace"));
+		openPreferencesDialog(ui);
+		selectWorkspacePreferences(ui);
 	}
 
+	private void openPreferencesDialog(IUIContext ui) throws WidgetSearchException {
+		/*
+		 * On the Mac, the preference menu item is in the application menu which is not visible
+		 * to the WT runtime.  To work around this, we invoke the menu via the associated hot-key.
+		 */
+		if (IS_MAC_OS)
+			ui.keyClick(WT.COMMAND, ',');
+		else
+			ui.click(new MenuItemLocator("Window/Preferences"));
+		ui.wait(new ShellShowingCondition("Preferences"));
+	}
+
+	private void selectWorkspacePreferences(IUIContext ui) throws WidgetSearchException {
+		ui.click(new FilteredTreeItemLocator("General/Workspace"));
+	}
+	
 	/*
 	 * (non-Javadoc)
 	 * 
