d65cfd1b36c949a42c8d7c804c15ff1e60b9d477
phil.quitslund <phil.quitslund@a2151643-f252-0410-9a6e-fd29858f4526>
2009-04-14 22:29:21 +0000

    New Handlers used to handle prompt when clobbering resources in a move

---
+++ helper-libs/eclipse34/plugins/org.wtc.eclipse.platform/src/org/wtc/eclipse/platform/shellhandlers/MovedResourceExistsShellHandler.java
@@ -0,0 +1,43 @@
+/**
+ * WT Commons Project 2008-2009
+ *
+ * http://code.google.com/p/wt-commons/wiki/WTSamples
+ */
+package org.wtc.eclipse.platform.shellhandlers;
+
+import junit.framework.TestCase;
+
+import com.windowtester.runtime.IUIContext;
+import com.windowtester.runtime.WidgetSearchException;
+import com.windowtester.runtime.swt.locator.ButtonLocator;
+
+/**
+ * Handler for the Resource Exists Dialog.  Click "Continue" to close if it is ever shown.
+ */
+public class MovedResourceExistsShellHandler extends AbstractContinueToCloseShellHandler {
+    /**
+     * Save the UI context.
+     *
+     * @param  ui  - Driver for UI generated input
+     */
+    public MovedResourceExistsShellHandler(IUIContext ui) {
+        super(ui, "Move Resources", true); //$NON-NLS-1$
+    }
+    
+    
+    /* (non-Javadoc)
+     * @see org.wtc.eclipse.platform.shellhandlers.AbstractButtonToCloseShellHandler#handle(com.windowtester.runtime.IUIContext)
+     */
+    @Override
+    public void handle(IUIContext ui) {
+    	//only handle if button is visible
+    	try {
+    		ButtonLocator continueButton = new ButtonLocator(getButtonLabel());
+			if (!continueButton.isVisible(ui))
+				return;
+		} catch (WidgetSearchException e) {
+			TestCase.fail(e.getMessage());
+		}
+    	super.handle(ui);
+    }
+}
