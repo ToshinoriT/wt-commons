06c52b700d135cfcf29d1e06f879eec45ba1603f
phil.quitslund <phil.quitslund@a2151643-f252-0410-9a6e-fd29858f4526>
2009-05-07 20:26:49 +0000

    Added a handler to dismiss the SVN Username prompt.  (This was failing test runs on Ubuntu with the Subversive SVN feature installed.)

--- trunk/helper-libs/eclipse34/plugins/org.wtc.eclipse.platform/src/org/wtc/eclipse/platform/tests/helpers/EditorHelperGoToLineTest.java
+++ trunk/helper-libs/eclipse34/plugins/org.wtc.eclipse.platform/src/org/wtc/eclipse/platform/tests/helpers/EditorHelperGoToLineTest.java
@@ -5,7 +5,7 @@
  */
 package org.wtc.eclipse.platform.tests.helpers;
 
-import com.windowtester.runtime.IUIContext;
+
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.wtc.eclipse.platform.PlatformActivator;
@@ -13,12 +13,36 @@ import org.wtc.eclipse.platform.helpers.EclipseHelperFactory;
 import org.wtc.eclipse.platform.helpers.IEditorHelper;
 import org.wtc.eclipse.platform.helpers.IResourceHelper;
 import org.wtc.eclipse.platform.helpers.ISimpleProjectHelper;
+import org.wtc.eclipse.platform.shellhandlers.UserNamePasswordShellHandler;
 import org.wtc.eclipse.platform.tests.EclipseUITest;
+
+import com.windowtester.runtime.IUIContext;
+import com.windowtester.runtime.swt.condition.shell.IShellMonitor;
 
 /**
  * Quick smoke test of the editor helper "Go to line" support.
  */
 public class EditorHelperGoToLineTest extends EclipseUITest {
+	
+	private final UserNamePasswordShellHandler passwordShellHandler = new UserNamePasswordShellHandler();
+	
+	@Override
+	protected void methodSetUp() throws Exception {
+		super.methodSetUp();
+		IUIContext ui = getUI();
+		IShellMonitor sm = (IShellMonitor) ui.getAdapter(IShellMonitor.class);
+		sm.add(passwordShellHandler);
+	}
+	
+	@Override
+	protected void methodTearDown() throws Exception {
+		super.methodTearDown();
+		IUIContext ui = getUI();
+		IShellMonitor sm = (IShellMonitor) ui.getAdapter(IShellMonitor.class);
+		sm.remove(passwordShellHandler);
+	}
+	
+	
     /**
      * Create a project, create a file, go to some lines.
      */
@@ -38,6 +62,7 @@ public class EditorHelperGoToLineTest extends EclipseUITest {
         resources.openFile(ui, fullFilePath);
 
         IEditorHelper editor = EclipseHelperFactory.getEditorHelper();
+        System.out.println("EditorHelperGoToLineTest.testGoToLine() -- pre goto"); //$NON-NLS-1$
         editor.gotoLine(ui, 4);
         editor.gotoLine(ui, 1);
     }
